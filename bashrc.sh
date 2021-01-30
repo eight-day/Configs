5d410541c5e0a12a984da9bd3f69342331ced00f
8 Day <keyboardwars@riseup.net>
2021-01-30 16:42:14 -0500

    Create bashrc.sh

---
+++ bashrc.sh
@@ -0,0 +1,48 @@
+PS1="[\033[1;00m\t\033[00m] \033[31m\w\033[1;00m\$";
+
+# Echo some hefty aliases
+alias ll='ls -l';
+alias la='ls -a';
+alias lla='ls -la';
+alias l='ls';
+alias lam='ls -A';
+alias llam='ls -lA';
+alias llr='ls -lR';
+alias llsa='ls -lsa';
+
+alias ns="netstat"
+alias py="python"
+alias mk="make"
+alias lz=ls"
+alias cls="clear"
+alias nm="nmap"
+alias rmf="rm -rf" # hehe
+
+if [ -f '/data/data/com.termux/files/usr/bin/bat']; then
+	alias ccat="bat";
+	echo "[*] All aliases set";
+else
+	echo "[*] Bat not found, install with: pkg install bat"
+fi
+
+export GREP_OPTIONS='--color=auto' GREP_COLOR='1;31';
+alias grep='grep $GREP_OPTIONS';
+
+flines () {
+	if [ -z "$arg1" ]; then
+		echo "No arguments passed.";
+		return 1;
+	fi
+	
+	flines=$(wc -l "$arg1" | awk '{ print $0; }');echo "$flines";
+	return flines;
+}
+
+
+function watchdir {
+	while true; do
+		lla;
+		sleep 2;
+		clear;
+	done
+}
